1b2c49011e7e600cdb4dbc989cb4766f1033790e
Deo Kalule <deograstius@gmail.com>
2020-11-08 15:01:33 -0500

    Created bg and bg-text GUI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg">
+      <div className="bg-image"></div>
+      <div className="bg-text">
+        <p>Mukyala</p>
+        <p>Opening May 2021</p>
+      </div>
     </div>
   );
 }
